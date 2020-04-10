7edf1fd82157eec025d21e722de2c8be3c4761cc
abitechcoder <abiolaolalekan39@gmail.com>
2020-04-10 19:31:00 +0100

    Writing a code to find documents in a collection

---
+++ findInterns.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+const findInterns = function(db, callback){
+    //Get the documents collection
+    db.collection('myMovies');
+}
+
+module.exports = findInterns;
